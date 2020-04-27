c32ab4465f7400ad7c00f67aa8f6cce8a623eb30
herrylegowo <herrylegowo19@gmail.com>
2020-04-28 02:46:02 +0800

    tools: GET, POST, PUT and DELETE

--- index.js
+++ index.js
@@ -22,24 +22,33 @@ app.get('/', (req, res) => {
     res.send('Hellow world!!!');
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
 
 // method GET
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The courses with the given ID was not fount');
+    if (!course) return res.status(404).send('The courses with the given ID was not fount');
     res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-    const result = Joi.validate(req.body, schema);
-    if (result.error) {
-        //    bed request (400)
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    // const schema = {
+    //     name: Joi.string().min(3).required()
+    // };
+    // const result = Joi.validate(req.body, schema);
+    // if (result.error) {
+    //     //    bed request (400)
+    //     res.status(400).send(result.error.details[0].message);
+    //     return;
+    // }
+    const {
+        error
+    } = validateCourse(req.body); //result error
+    if (error)
+        return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length + 1,
@@ -48,6 +57,44 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send('The courses with the given ID was not fount');
+
+    // validate
+    // const result = validateCourse(req.body);
+    const {
+        error
+    } = validateCourse(req.body); //result error
+    if (error)
+        return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course, schema);
+}
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send('The courses with the given ID was not fount');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+});
+
+
+
 
 // port variable global
 const port = process.env.PORT || 3000;
